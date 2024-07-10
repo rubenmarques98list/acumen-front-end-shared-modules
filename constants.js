0457e2f33686baca5cdd13e07583d63d5d6af807
Ruben Marques <ruben.marques@list.lu>
2024-07-10 13:50:45 +0200

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,14 +1,20 @@
-export default class Constants{
+export default class Constants {
 
     constructor(envContext) {
         this.envContext = envContext;
         console.log(this.envContext);
-    }
 
-    acumenApplicationTitle = this.envContext.VITE_ACUMEN_APPLICATION_TITLE;
-    apiGatewayProtocol = this.envContext.VITE_ACUMEN_API_GATEWAY_PROTOCOL;
-    apiGatewayHostname = window.location.hostname;
-    apiGatewayPort = this.envContext.VITE_ACUMEN_API_GATEWAY_PORT;
+        this.acumenApplicationTitle = this.envContext.VITE_ACUMEN_APPLICATION_TITLE;
+        this.apiGatewayProtocol = this.envContext.VITE_ACUMEN_API_GATEWAY_PROTOCOL;
+        this.apiGatewayHostname = window.location.hostname;
+        this.apiGatewayPort = this.envContext.VITE_ACUMEN_API_GATEWAY_PORT;
+
+        this.loginEndpoint = this.envContext.VITE_LOGIN_ENDPOINT;
+        this.refreshTokenEndpoint = this.envContext.VITE_REFRESH_TOKEN_ENDPOINT
+        this.logoutEndpoint = this.envContext.VITE_LOGOUT_ENDPOINT;
+        this.adminServicesEndpoint = this.envContext.VITE_ADMIN_SERVICES_ENDPOINT
+        this.authAccessRightsRouteID = this.envContext.VITE_AUTH_ACCESS_RIGHTS_ROUTE_ID
+    }
 
     getApplicationTitle() {
         return import.meta.env.VITE_ACUMEN_APPLICATION_TITLE
@@ -18,29 +24,25 @@ export default class Constants{
         return `${this.apiGatewayProtocol}://${this.apiGatewayHostname}:${this.apiGatewayPort}`
     }
 
-    loginEndpoint = this.envContext.VITE_LOGIN_ENDPOINT;
-
     getLoginEndpointUrl() {
         return `${this.getApiUrl()}${this.loginEndpoint}`;
     }
-
-    refreshTokenEndpoint = this.envContext.VITE_REFRESH_TOKEN_ENDPOINT
 
     getRefreshTokenEndpointUrl() {
         return `${this.getApiUrl()}${this.refreshTokenEndpoint}`;
     }
 
-    logoutEndpoint = this.envContext.VITE_LOGOUT_ENDPOINT;
+    
 
     getLogoutEndpointURL() {
         return `${this.getApiUrl()}${this.logoutEndpoint}`;
     }
 
-    adminServicesEndpoint = this.envContext.VITE_ADMIN_SERVICES_ENDPOINT
+    
 
     getAdminServicesEndpointUrl() {
         return `${this.getApiUrl()}${this.adminServicesEndpoint}`
     }
 
-    authAccessRightsRouteID = this.envContext.VITE_AUTH_ACCESS_RIGHTS_ROUTE_ID
+    
 }
