67720077413848e17ed951d461b7cd9bed14f44c
Ruben Marques <ruben.marques@list.lu>
2024-07-10 11:33:22 +0200

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,36 @@
+export default {
+    acumenApplicationTitle: import.meta.env.VITE_ACUMEN_APPLICATION_TITLE,
+    apiGatewayProtocol: import.meta.env.VITE_ACUMEN_API_GATEWAY_PROTOCOL,
+    apiGatewayHostname: window.location.hostname,
+    apiGatewayPort: import.meta.env.VITE_ACUMEN_API_GATEWAY_PORT,
+    
+    getApiUrl: function() {
+        return `${this.apiGatewayProtocol}://${this.apiGatewayHostname}:${this.apiGatewayPort}`;
+    },
+
+    loginEndpoint: import.meta.env.VITE_LOGIN_ENDPOINT,
+
+    getLoginEndpointUrl: function() {
+        return `${this.getApiUrl()}${this.loginEndpoint}`;
+    },
+
+    refreshTokenEndpoint: import.meta.env.VITE_REFRESH_TOKEN_ENDPOINT,
+
+    getRefreshTokenEndpointUrl: function() {
+        return `${this.getApiUrl()}${this.refreshTokenEndpoint}`;
+    },
+
+    logoutEndpoint: import.meta.env.VITE_LOGOUT_ENDPOINT,
+
+    getLogoutEndpointURL: function() {
+        return `${this.getApiUrl()}${this.logoutEndpoint}`;
+    },
+
+    adminServicesEndpoint: import.meta.env.VITE_ADMIN_SERVICES_ENDPOINT,
+
+    getAdminServicesEndpointUrl: function() {
+        return `${this.getApiUrl()}${this.adminServicesEndpoint}`
+    },
+
+    authAccessRightsRouteID: import.meta.env.VITE_AUTH_ACCESS_RIGHTS_ROUTE_ID,
+}
