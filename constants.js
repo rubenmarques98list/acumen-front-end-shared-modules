b3fb9095b402afdce71745d9c4c43167e4414988
Ruben Marques <ruben.marques@list.lu>
2024-07-10 13:40:37 +0200

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,40 +1,45 @@
-export default {
-    // acumenApplicationTitle: import.meta.env.VITE_ACUMEN_APPLICATION_TITLE,
-    /* apiGatewayProtocol: import.meta.env.VITE_ACUMEN_API_GATEWAY_PROTOCOL,
-    apiGatewayHostname: window.location.hostname,
-    apiGatewayPort: import.meta.env.VITE_ACUMEN_API_GATEWAY_PORT, */
+export default class Constants{
 
-    getApplicationTitle: function() {
+    constructor(envContext) {
+        this.envContext = envContext;
+    }
+
+    acumenApplicationTitle = this.envContext.VITE_ACUMEN_APPLICATION_TITLE;
+    apiGatewayProtocol = this.envContext.VITE_ACUMEN_API_GATEWAY_PROTOCOL;
+    apiGatewayHostname = window.location.hostname;
+    apiGatewayPort = this.envContext.VITE_ACUMEN_API_GATEWAY_PORT;
+
+    getApplicationTitle() {
         return import.meta.env.VITE_ACUMEN_APPLICATION_TITLE
-    },
+    }
     
-    /* getApiUrl: function() {
-        return `${this.apiGatewayProtocol}://${this.apiGatewayHostname}:${this.apiGatewayPort}`;
-    },
+    getApiUrl() {
+        return `${this.apiGatewayProtocol}://${this.apiGatewayHostname}:${this.apiGatewayPort}`
+    }
 
-    loginEndpoint: import.meta.env.VITE_LOGIN_ENDPOINT,
+    loginEndpoint = this.envContext.VITE_LOGIN_ENDPOINT;
 
-    getLoginEndpointUrl: function() {
+    getLoginEndpointUrl() {
         return `${this.getApiUrl()}${this.loginEndpoint}`;
-    },
+    }
 
-    refreshTokenEndpoint: import.meta.env.VITE_REFRESH_TOKEN_ENDPOINT,
+    refreshTokenEndpoint = this.envContext.VITE_REFRESH_TOKEN_ENDPOINT
 
-    getRefreshTokenEndpointUrl: function() {
+    getRefreshTokenEndpointUrl() {
         return `${this.getApiUrl()}${this.refreshTokenEndpoint}`;
-    },
+    }
 
-    logoutEndpoint: import.meta.env.VITE_LOGOUT_ENDPOINT,
+    logoutEndpoint = this.envContext.VITE_LOGOUT_ENDPOINT;
 
-    getLogoutEndpointURL: function() {
+    getLogoutEndpointURL() {
         return `${this.getApiUrl()}${this.logoutEndpoint}`;
-    },
+    }
 
-    adminServicesEndpoint: import.meta.env.VITE_ADMIN_SERVICES_ENDPOINT,
+    adminServicesEndpoint = this.envContext.VITE_ADMIN_SERVICES_ENDPOINT
 
-    getAdminServicesEndpointUrl: function() {
+    getAdminServicesEndpointUrl() {
         return `${this.getApiUrl()}${this.adminServicesEndpoint}`
-    },
+    }
 
-    authAccessRightsRouteID: import.meta.env.VITE_AUTH_ACCESS_RIGHTS_ROUTE_ID, */
+    authAccessRightsRouteID = this.envContext.VITE_AUTH_ACCESS_RIGHTS_ROUTE_ID
 }
